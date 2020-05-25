43f5efad5cdb189f9545933697ed025f2d0de07d
Robin Scherrer <mail@robinscherrer.com>
2020-05-25 20:41:23 +0200

    better function and var naming

--- helminator.py
+++ helminator.py
@@ -6,79 +6,78 @@ from slack.errors import SlackApiError
 from pathlib import Path
 import os
 
-chart_repos = []
-mycharts = []
-new_updates = []
+helm_repo_charts = []
+ansible_helm_charts = []
+chart_updates = []
 
-rootdir = os.environ.get("HELMINATOR_ROOT_DIR")
-client = WebClient(token=os.environ['HELMINATOR_SLACK_API_TOKEN'])
-channel = os.environ.get("HELMINATOR_SLACK_CHANNEL")
+search_dir = os.environ.get("HELMINATOR_ROOT_DIR")
+slack_client = WebClient(token=os.environ['HELMINATOR_SLACK_API_TOKEN'])
+slack_channel = os.environ.get("HELMINATOR_SLACK_CHANNEL")
 
 
-def get_helm_charts(path):
+def get_ansible_helm(path):
     with open(path) as stream:
-        ymlfile = yaml.safe_load(stream)
+        tasks = yaml.safe_load(stream)
 
-    for i in ymlfile:
-        if i.get('community.kubernetes.helm'):
-            if not any(chart for chart in mycharts if chart == i['community.kubernetes.helm']['chart_ref']):
-                segments = i['community.kubernetes.helm']['chart_ref'].split('/')
+    for task in tasks:
+        if task.get('community.kubernetes.helm'):
+            if not any(chart for chart in ansible_helm_charts if chart == task['community.kubernetes.helm']['chart_ref']):
+                segments = task['community.kubernetes.helm']['chart_ref'].split('/')
                 if len(segments) > 2:
                     continue
                 chart = {
                     'name': segments[-1],
-                    'version': i['community.kubernetes.helm'].get('chart_version')
+                    'version': task['community.kubernetes.helm'].get('chart_version')
                 }
+                ansible_helm_charts.append(chart)
 
-                mycharts.append(chart)
 
-
-def get_repo_urls(path):
+def get_repo_charts(path):
     with open(path) as stream:
-        ymlfile = yaml.safe_load(stream)
+        tasks = yaml.safe_load(stream)
 
-    for i in ymlfile:
-        if i.get('community.kubernetes.helm_repository'):
-            if not any(repo for repo in chart_repos if repo['name'] == i['community.kubernetes.helm_repository']['name']):
+    for task in tasks:
+        if task.get('community.kubernetes.helm_repository'):
+            if not any(repo for repo in helm_repo_charts if repo['name'] == task['community.kubernetes.helm_repository']['name']):
                 repo = {
-                    'name': i['community.kubernetes.helm_repository']['name'],
-                    'url': i['community.kubernetes.helm_repository']['repo_url']
+                    'name': task['community.kubernetes.helm_repository']['name'],
+                    'url': task['community.kubernetes.helm_repository']['repo_url']
                 }
 
-                response = requests.get(url=f"{repo['url']}/index.yaml")
-                chartyml = yaml.safe_load(response.content)
+                repo_response = requests.get(url=f"{repo['url']}/index.yaml")
+                repo_charts = yaml.safe_load(repo_response.content)
 
-                for charts in chartyml['entries'].items():
+                for repo_charts in repo_charts['entries'].items():
                     latest_version = "0.0.1"
-                    for chart in charts[1]:
-                        if not any(c for c in mycharts if c['name'] == chart['name']):
+                    for repo_chart in repo_charts[1]:
+                        if not any(c for c in ansible_helm_charts if c['name'] == repo_chart['name']):
                             continue
-                        if not semver.VersionInfo.isvalid(chart['version']):
+                        if not semver.VersionInfo.isvalid(repo_chart['version']):
                             continue
-                        ansible_version = [charts['version'] for charts in mycharts if charts['name'] == chart['name']]
+                        ansible_version = [charts['version'] for charts in ansible_helm_charts if charts['name'] == repo_chart['name']]
                         if not semver.VersionInfo.isvalid(ansible_version[0]):
                             continue
-                        if semver.match(f"{ansible_version[0]}", f">={chart['version']}"):
+                        if semver.match(f"{ansible_version[0]}", f">={repo_chart['version']}"):
                             continue
-                        if semver.match(f"{latest_version}", f">={chart['version']}"):
+                        if semver.match(f"{latest_version}", f">={repo_chart['version']}"):
                             continue
-                        latest_version = chart['version']
-                        chart = {
-                            'name': chart['name'],
+                        latest_version = repo_chart['version']
+                        repo_chart = {
+                            'name': repo_chart['name'],
                             'new_version': latest_version
                         }
-                        new_updates.append(chart)
-                chart_repos.append(repo)
+                        chart_updates.append(repo_chart)
+                helm_repo_charts.append(repo)
 
 
-for item in Path(rootdir).glob("**/*"):
+for item in Path(search_dir).glob("**/*"):
     if not item.is_file():
         continue
     if item.suffix not in ['.yml']:
         continue
-    get_helm_charts(path=item.absolute())
+    get_ansible_helm(path=item.absolute())
 
-for item in Path(rootdir).glob("**/*"):
+for item in Path(search_dir).glob("**/*"):
     if not item.is_file():
         continue
     excludes = ['prometheus-operator', 'minio-backup', 'local-path-provisioner', 'nfs-client-provisioner']
@@ -86,13 +85,13 @@ for item in Path(rootdir).glob("**/*"):
         continue
     if item.suffix not in ['.yml']:
         continue
-    get_repo_urls(path=item.absolute())
+    get_repo_charts(path=item.absolute())
 
-if new_updates:
-    text = [f"Update for chart `{update['name']}` available: version `{update['new_version']}`" for update in new_updates]
+if chart_updates:
+    text = [f"Update for chart `{update['name']}` available: version `{update['new_version']}`" for update in chart_updates]
     try:
-        response = client.chat_postMessage(
-            channel=channel,
+        response = slack_client.chat_postMessage(
+            channel=slack_channel,
             text='\n'.join(text))
     except SlackApiError as e:
         assert e.response["ok"] is False
