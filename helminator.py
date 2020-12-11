a9f329638d6361f8580f1fa98658c486b1ccdc7d
gi8 <giottolino@gmail.com>
2020-12-11 11:19:18 +0100

    correct imports

--- helminator.py
+++ helminator.py
@@ -17,8 +17,7 @@ try:
     import yaml
     from gitlab import Gitlab as Gitlab_cli
     from gitlab.exceptions import GitlabCreateError, GitlabGetError
-    from gitlab.v4.objects import (Project, ProjectBranch, ProjectCommit,
-                                   ProjectMergeRequest)
+    from gitlab.v4.objects import Project, ProjectBranch, ProjectCommit, ProjectMergeRequest
     from slack import WebClient
     from slack.errors import SlackApiError
 except Exception:
